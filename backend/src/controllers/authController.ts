186aa5c0b735c447d7fef9cd139fa8eddfc2e82e
Alexander Malko <alexsandr77867@gmail.com>
2024-05-14 20:18:06 +0400

    create default backend function

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+const registerUser = (req: Request, res: Response) => {};
+const authenticateUser = (req: Request, res: Response) => {};
+const logoutUser = (req: Request, res: Response) => {};
+
+export { registerUser, authenticateUser, logoutUser };

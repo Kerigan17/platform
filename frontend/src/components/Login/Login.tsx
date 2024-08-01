9939d506a5d397a7bbc9305100969ed31d8f1be8
alexandr <alexsandr77867@gmail.com>
2024-08-01 21:36:41 +0400

    add remember me

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -7,12 +7,17 @@ function Login() {
 
             <form className="login__form" action="">
                 <div className='form__group'>
-                    <img className='form__img' src='../../../public/login/mail.svg' alt=""/>
+                    <img className='form__img' src='../../../public/images/login/mail.svg' alt=""/>
                     <input className='form__field' id='login' type="email"/>
                 </div>
                 <div className='form__group'>
-                    <img className='form__img' src="../../../public/login/password.svg" alt=""/>
+                    <img className='form__img' src="../../../public/images/login/password.svg" alt=""/>
                     <input className='form__field' id='password' type="password"/>
+                </div>
+                <div className="form__group remember">
+                    <input className='form__remember' id='remember' type="checkbox"/>
+                    <label htmlFor="remember"></label>
+                    <label >Remember me</label>
                 </div>
                 <button className='form__btn'>Login</button>
             </form>
